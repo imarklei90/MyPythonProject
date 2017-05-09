8e4f3aa5de97578599975c3c58bb75581e88051e
leiguan <imarklei90@126.com>
2017-05-09 18:07:53 +0800

    Test a Simple Crawler

--- src/Crawler/SimpleCrawler.py
+++ src/Crawler/SimpleCrawler.py
@@ -6,5 +6,5 @@ __author__ = 'leiguan'
 
 import urllib2
 
-response = urllib2.urlopen("http://www.dlworld.cn/YeJieDongTai/3860.html")
+response = urllib2.urlopen("https://zhuanlan.zhihu.com/p/26799224?group_id=845278956881055744")
 print response.read()
