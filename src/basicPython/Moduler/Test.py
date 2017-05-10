1f814d44a3d24c50dae82a2a93e818ba39b447cb
leiguan <imarklei90@126.com>
2017-05-10 17:17:54 +0800

    Add Some Basic Python Code about Moduler

---
+++ src/basicPython/Moduler/Test.py
@@ -0,0 +1,8 @@
+def hello():
+    print "Hello Moduler"
+
+def test():
+    hello()
+"""使用条件测试代码的模块"""
+if __name__ == '__main__':
+    test()
---
+++ src/basicPython/Moduler/TestModuler.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append("E:\projects\MyPythonProject\src\\basicPython\Moduler")
+
+import Test
+Test.test()
