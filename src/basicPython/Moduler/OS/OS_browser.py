38ca6bcc9d1e46ab96a1634a2bec85fc8f27692c
leiguan <imarklei90@126.com>
2017-05-11 11:29:15 +0800

    Add Some Basic Python Code about Moduler

---
+++ src/basicPython/Moduler/OS/OS_browser.py
@@ -0,0 +1,9 @@
+#-*- encoding:utf-8-*-
+'''
+windows专有的启动文件的方式
+'''
+import src.basicPython.Moduler.OS
+src.basicPython.Moduler.OS.startfile(r'E:\Programs\Devs\VirtualBox\VirtualBox.exe')
+
+import webbrowser
+webbrowser.open("www.python.org")
