5c91d1e9eb0ec6443680a5d898c4e0d9f31d83d2
leiguan <imarklei90@126.com>
2017-05-16 14:32:27 +0800

    Add Some Basic Python Code About Crawler

--- src/Crawler/bs4/demo/CrawlerTaoBaoSearchResult.py
+++ src/Crawler/bs4/demo/CrawlerTaoBaoSearchResult.py
@@ -11,7 +11,6 @@ import re
 def getTaoBaoSearchInfo(url):
     try:
         r = requests.get(url, timeout = 30)
-        print r.status_code
         r.raise_for_status()
         r.encoding = r.apparent_encoding
         return r.text
@@ -21,12 +20,13 @@ def getTaoBaoSearchInfo(url):
 
 def parseTaoBaoSearchInfo(infoLists, html):
     try:
-        title = re.findall(r'\"title\"\:\"\.*?"',html)
+        title = re.findall(r'\"title\"\:\"\.*?"', html)
         price = re.findall(r'\"price\"\:\"[\d\.]*\"', html)
         for i in range(len(title)):
             title = eval(title[i].split(':')[1])
             price = eval(price[i].split(':')[1])
-            infoLists.append([title,price])
+            print "------" + title + price
+            infoLists.append([title, price])
     except:
         print ""
 
@@ -35,13 +35,15 @@ def printTaoBaoSearchInfo(infoLists):
     tplt = '{:4}\t{:16}\t{:8}'
     print tplt.format("序号","名称","价格")
     count = 0
+
     for i in infoLists:
         count = count + 1
-        print tplt.format(count, infoLists[0],infoLists[1])
+        print "infoLists:" + i
+        print tplt.format(count, i[0],i[1])
 
 
 def main():
-    searchData = "笔记本电脑"
+    searchData = '笔记本电脑'
     depth = 3
     start_url = "https://s.taobao.com/search?q=" + searchData
     infoLists = []
