61fd400c7c69bf528ffe7b7fb01617f266cf043f
leiguan <imarklei90@126.com>
2017-05-25 18:19:45 +0800

    Add normalization to kNN

--- src/MachineLearning/kNN/kNN.py
+++ src/MachineLearning/kNN/kNN.py
@@ -1,7 +1,9 @@
 # -*- encoding:utf-8 -*-
 
 '''
-    kNN算法实现对电影进行分类
+    kNN算法实现:
+        对电影进行分类
+        改进约会网站的效果
 '''
 
 from numpy import *
@@ -68,6 +70,20 @@ def createMatplotlib(datingDataMat, datingLabels):
     plt.show()
 
 
+# 归一化特征值(用于处理不同范围内的特征值,降低不同特征值的严重影响)
+# newValue = (oldValue - min) / (max - min)
+def autoNum(dataSet):
+    minVals = dataSet.min(0)  # 加上参数 0 使得函数可以从列中选取最小值,而不是选取当前行的最小值
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals  # 函数计算可能的取值范围
+    normalDataSet = zeros(shape(dataSet))  # 创建新的返回矩阵
+    m = dataSet.shape[0]
+    #  使用tile将变量内容复制成输入矩阵同样大小的矩阵
+    normalDataSet = dataSet - tile(minVals, (m, 1))
+    normalDataSet = normalDataSet / tile(ranges, (m, 1))  # 具体特征值相除
+    return normalDataSet, ranges, minVals
+
+
 if __name__ == '__main__':
     group, labels = createDataSet()
     # print group
@@ -79,4 +95,9 @@ if __name__ == '__main__':
     # print datingDataMat
     # print datingLabels[0:20]
 
-    createMatplotlib(datingDataMat, datingLabels)
+    # createMatplotlib(datingDataMat, datingLabels)
+
+    norMat, ranges, minVals = autoNum(datingDataMat)
+    print norMat
+    print ranges
+    print minVals
