5235db278174cb45118756a7ad94b599fdada33c
leiguan <imarklei90@126.com>
2017-05-25 20:24:03 +0800

    Add Some Basic Python Code

--- src/MachineLearning/kNN/kNN.py
+++ src/MachineLearning/kNN/kNN.py
@@ -4,6 +4,8 @@
     kNN算法实现:
         对电影进行分类
         改进约会网站的效果
+    使用Matplotlib创建散点图
+    归一化数据
 '''
 
 from numpy import *
@@ -83,6 +85,21 @@ def autoNum(dataSet):
     normalDataSet = normalDataSet / tile(ranges, (m, 1))  # 具体特征值相除
     return normalDataSet, ranges, minVals
 
+# 分类器针对约会网站的测试
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNum(datingDataMat)
+    m = norMat.shape[0]
+    numTestVces = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVces):
+        classifierResult = classify0(normMat[i:], normMat[numTestVces:m, :], datingLabels[numTestVces:m],3)
+        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print "the total error rate is : %f" % (errorCount / float(numTestVces))
+
 
 if __name__ == '__main__':
     group, labels = createDataSet()
@@ -98,6 +115,8 @@ if __name__ == '__main__':
     # createMatplotlib(datingDataMat, datingLabels)
 
     norMat, ranges, minVals = autoNum(datingDataMat)
-    print norMat
-    print ranges
-    print minVals
+    # print norMat
+    # print ranges
+    # print minVals
+
+    datingClassTest()
